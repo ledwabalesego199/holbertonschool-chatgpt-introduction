ed34bb9dce5c7bc67bfdbd27f68b9b3e01cea532
Nonhle_LSG <ledwabalesego199@gmail.com>
2025-06-20 10:26:59 +0200

    Update print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,12 @@
+$ cat print_arguments.py
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.argv)):
+for i in range(len(sys.argv)):
     print(sys.argv[i])
+
+$ ./print_arguments.py 1 2 3
+print_arguments.py
+1
+2
+3
